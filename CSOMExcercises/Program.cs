8314b13ad8753b00780768f207842d6a1f574dd6
Nghiep Tran Dai <nghiep.tran.dai@preciofishbone.se>
2021-10-15 17:07:19 +0700

    Initial commit

---
+++ CSOMExcercises/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Security;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace CSOMExcercises
+{
+    class Program
+    {
+        public static IConfiguration GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, true);
+            return builder.Build();
+        }
+        public static SecureString ConvertToSecureString(string password)
+        {
+            if (password == null)
+            {
+                throw new ArgumentNullException("Missing password");
+            }
+
+            var securePassword = new SecureString();
+            foreach (char c in password)
+            {
+                securePassword.AppendChar(c);
+            }
+
+            securePassword.MakeReadOnly();
+            return securePassword;
+        }
+        public static async Task Main(string[] args)
+        {
+            var configurations = GetConfiguration();
+            var sharepointConfigs = configurations.GetSection("SharePoint").Get<SharePointConfiguration>();
+            /*Console.WriteLine(sharepointConfigs.SiteUri);*/
+            
+            // Note: The PnP Sites Core AuthenticationManager class also supports this
+            using (var authenticationManager = new AuthenticationManager())
+            using (var context = authenticationManager.GetContext(sharepointConfigs))
+            {
+                context.Load(context.Web, p => p.Title);
+                await context.ExecuteQueryAsync();
+                Console.WriteLine($"Title: {context.Web.Title}");
+            }
+        }
+    }
+}
---
+++ CSOMExcercises/SharePointConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSOMExcercises
+{
+    public class SharePointConfiguration
+    {
+        public string SiteUri { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
